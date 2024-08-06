0a202f1fdc89c30ddafd14a511fe89783b345529
SebaRodriguezzz <sebas.r1070@gmail.com>
2024-08-05 23:44:54 -0300

    Add home controller and view

---
+++ src/test/java/bondio/BondioCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package bondio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BondioCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
