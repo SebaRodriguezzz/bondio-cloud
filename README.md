921bd9a84b81f835fa039cc49ffc573423b352b2
SebaRodriguezzz <sebas.r1070@gmail.com>
2024-08-05 21:22:12 -0300

    Start project

---
+++ README.md
@@ -0,0 +1,1 @@
+# bondio-cloud
---
+++ src/main/java/bondio/BondioCloudApplication.java
@@ -0,0 +1,13 @@
+package bondio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BondioCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BondioCloudApplication.class, args);
+    }
+
+}
