225a4551822c4197fe240b8b03ccf8e02e781b22
SebaRodriguezzz <sebas.r1070@gmail.com>
2024-08-08 23:23:51 -0300

    Add validations to entities

--- src/main/java/bondio/persistence/entity/Bondio.java
+++ src/main/java/bondio/persistence/entity/Bondio.java
@@ -1,5 +1,7 @@
 package bondio.persistence.entity;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.List;
@@ -7,7 +9,10 @@ import java.util.List;
 @Data
 public class Bondio {
 
+    @NotNull
+    @Size(min = 3, message = "Name must be at least 3 characters long")
     private String name;
+    @Size(min = 1, message = "You must choose at least 1 ingredient")
     private List<Ingredient> ingredients;
 
 }
