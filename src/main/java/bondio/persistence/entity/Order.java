bc97781567a9d5ed3aaf2c62216e9a6577eeb938
SebaRodriguezzz <sebas.r1070@gmail.com>
2024-08-11 19:54:28 -0300

    Implement JPA

--- src/main/java/bondio/persistence/entity/Order.java
+++ src/main/java/bondio/persistence/entity/Order.java
@@ -1,23 +1,23 @@
 package bondio.persistence.entity;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Data
-@Table
+@Entity
 public class Order {
 
     private static final long serialVersionUID = 1L;
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private Date placedAt;
     @NotBlank(message = "Delivery name is required")
@@ -36,7 +36,7 @@ public class Order {
     private String ccExpiration;
     @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
-
+    @OneToMany(cascade = CascadeType.ALL)
     private List<Bondio> bondios = new ArrayList<>();
     public void addBondio(Bondio bondio) {
         this.bondios.add(bondio);
