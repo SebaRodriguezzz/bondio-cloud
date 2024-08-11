42a247d9eed18c21287e2c375069e12fb32a45cb
SebaRodriguezzz <sebas.r1070@gmail.com>
2024-08-10 22:16:34 -0300

    Add schema and data files

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+delete from Ingredient_Ref;
+delete from Taco;
+delete from Taco_Order;
+delete from Ingredient;
+insert into Ingredient (id, name, type) values ('CBTA', 'Ciabatta', 'BREAD');
+insert into Ingredient (id, name, type) values ('WHTB', 'White Bread', 'BREAD');
+insert into Ingredient (id, name, type) values ('SALT', 'Salt', 'SEASONING');
+insert into Ingredient (id, name, type) values ('PEPP', 'Pepper', 'SEASONING');
+insert into Ingredient (id, name, type) values ('TOMA', 'Tomato', 'VEGGIES');
+insert into Ingredient (id, name, type) values ('ONIO', 'Onion', 'VEGGIES');
+insert into Ingredient (id, name, type) values ('CHED', 'Cheddar', 'CHEESE');
+insert into Ingredient (id, name, type) values ('PROV', 'Provolone', 'CHEESE');
+insert into Ingredient (id, name, type) values ('YLLW', 'Yellow Mustard', 'SAUCE');
+insert into Ingredient (id, name, type) values ('MAYO', 'Mayonnaise', 'SAUCE');
+
