5a6f01a48847b1965ddd1ae630b701cca29a2682
SebaRodriguezzz <sebas.r1070@gmail.com>
2024-08-11 19:02:24 -0300

    Implement Spring Data JDBC

--- src/main/java/bondio/persistence/repository/OrderRepository.java
+++ src/main/java/bondio/persistence/repository/OrderRepository.java
@@ -1,7 +1,6 @@
 package bondio.persistence.repository;
 
 import bondio.persistence.entity.Order;
+import org.springframework.data.repository.CrudRepository;
 
-public interface OrderRepository {
-    Order save(Order order);
-}
+public interface OrderRepository extends CrudRepository<Order, Long> {}
