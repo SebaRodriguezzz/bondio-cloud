5fa052a7207b7dbc5c30dddecfc82d50f9389e0b
SebaRodriguezzz <sebas.r1070@gmail.com>
2024-08-13 23:16:29 -0300

    Change crud repository parameter to UUID

--- src/main/java/bondio/persistence/repository/OrderRepository.java
+++ src/main/java/bondio/persistence/repository/OrderRepository.java
@@ -4,8 +4,9 @@ import bondio.persistence.entity.Order;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.UUID;
 
-public interface OrderRepository extends CrudRepository<Order, Long> {
+public interface OrderRepository extends CrudRepository<Order, UUID> {
 
     List<Order> findByDeliveryZip(String deliveryZip);
 }
